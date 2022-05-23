f84138cb982537a461226f570a12c7250d9ca9c8
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-05-23 12:16:16 -0300

    registrar usuario formularios reactivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Modulos
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -22,7 +26,8 @@ import { SpinnerComponent } from './shared/spinner/spinner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar-usuario.component.css']
 })
 export class RegistrarUsuarioComponent implements OnInit {
+  registrarUsuario: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.registrarUsuario = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      repetirPassword: ['', Validators.required],
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  registrar() {
+    const email = this.registrarUsuario.value.email;
+    const password = this.registrarUsuario.value.password;
+    const repetirPassowrd = this.registrarUsuario.value.repetirPassword;
+    console.log(email, password, repetirPassowrd);
   }
 
 }
