492ae472e443648e16a2aca1838843c4a981027e
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-05-23 15:58:19 -0300

    recuperar password

--- src/app/components/recuperar-password/recuperar-password.component.ts
+++ src/app/components/recuperar-password/recuperar-password.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FirebaseCodeErrorService } from 'src/app/services/firebase-code-error.service';
 
 @Component({
   selector: 'app-recuperar-password',
   templateUrl: './recuperar-password.component.html',
-  styleUrls: ['./recuperar-password.component.css']
+  styleUrls: ['./recuperar-password.component.css'],
 })
 export class RecuperarPasswordComponent implements OnInit {
+  recuperarUsuario: FormGroup;
+  loading: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    private firebaseError: FirebaseCodeErrorService
+  ) {
+    this.recuperarUsuario = this.fb.group({
+      correo: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+
+  recuperar() {
+    const email = this.recuperarUsuario.value.correo;
+
+    this.loading = true;
+    this.afAuth
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        this.toastr.info('Le enviamos un correo para restablecer su password', 'Recuperar Password')
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        this.loading = false;
+        this.toastr.error(this.firebaseError.codeError(error.code), 'Error');
+      });
+  }
 }
