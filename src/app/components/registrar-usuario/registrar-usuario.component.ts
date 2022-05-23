e6e1f805bf66ef9ce6f4a62cccf1da9a1d65ee54
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-05-23 14:31:26 -0300

    agregamos mensajes de exito, erorr y loading

--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-usuario',
@@ -10,8 +11,14 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegistrarUsuarioComponent implements OnInit {
   registrarUsuario: FormGroup;
+  loading: boolean = false;
 
-  constructor(private fb: FormBuilder, private afAuth: AngularFireAuth, private toastr: ToastrService) {
+  constructor(
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
     this.registrarUsuario = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
@@ -26,14 +33,24 @@ export class RegistrarUsuarioComponent implements OnInit {
     const password = this.registrarUsuario.value.password;
     const repetirPassowrd = this.registrarUsuario.value.repetirPassword;
 
+    if (password !== repetirPassowrd) {
+      this.toastr.error(
+        'Las contraseñas ingresadas deben ser las mismas',
+        'Error'
+      );
+      return;
+    }
 
+    this.loading = true;
     this.afAuth
       .createUserWithEmailAndPassword(email, password)
-      .then((user) => {
-        console.log(user);
+      .then(() => {
+        this.loading = false;
+        this.toastr.success('El usuario fue registrado con exito!', 'Usuario registrado');
+        this.router.navigate(['/login']);
       })
       .catch((error) => {
-        console.log(error);
+        this.loading = false;
         this.toastr.error(this.firebaseError(error.code), 'Error');
       });
   }
