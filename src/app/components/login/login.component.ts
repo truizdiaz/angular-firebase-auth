b03bbc8b4f5274751ea4c83eb56ff4f47bd0c40e
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-05-23 15:08:15 -0300

    login HTML

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginUsuario: FormGroup;
+  loading: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+    this.loginUsuario = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
   }
 
+  ngOnInit(): void {}
+
+  login() {
+    const email = this.loginUsuario.value.email;
+    const password = this.loginUsuario.value.password;
+
+    this.loading = true;
+    this.afAuth.signInWithEmailAndPassword(email, password).then((user) => {
+      this.router.navigate(['/dashboard']);
+    }).catch((error) => {
+      this.loading = false;
+      console.log(error);
+    })
+  }
 }
