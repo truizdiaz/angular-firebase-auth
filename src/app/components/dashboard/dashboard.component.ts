21cd9cbb03715b75eb6f3a854c451b3acc18079e
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-05-24 09:34:32 -0300

    dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  dataUser: any;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth,
+      private router: Router) { }
 
   ngOnInit(): void {
+    this.afAuth.currentUser.then(user => {
+      if(user && user.emailVerified) {
+        this.dataUser = user;
+        console.log(user)
+      } else {
+        this.router.navigate(['/login']);
+      }
+    })
+  }
+
+  logOut() {
+    this.afAuth.signOut().then(() => this.router.navigate(['/login']));
   }
 
 }
